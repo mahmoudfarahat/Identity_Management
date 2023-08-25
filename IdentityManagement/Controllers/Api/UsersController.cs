3d95cdf270c7f4188db7b57324266be8ee12e6e4
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-08-25 16:25:12 +0300

    1.2

---
+++ IdentityManagement/Controllers/Api/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using IdentityManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManagement.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+ 
+
+        public UsersController(UserManager<ApplicationUser> userManager )
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+                return NotFound();
+            await userManager.DeleteAsync(user);
+
+            return Ok();
+        }
+
+    }
+}
--- IdentityManagement/Controllers/UsersController.cs
+++ IdentityManagement/Controllers/UsersController.cs
@@ -133,6 +133,100 @@ namespace IdentityManagement.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(AddUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
 
+            if(!model.Roles.Any(r => r.IsSelected))
+            {
+                ModelState.AddModelError("Roles", "Please select at least one role");
+                return View(model);
+            }
+            if (await userManager.FindByEmailAsync(model.Email) != null)
+            {
+                ModelState.AddModelError("Email", "Email is already exists");
+                return View(model);
+            }
+            if (await userManager.FindByNameAsync(model.Username) != null)
+            {
+                ModelState.AddModelError("Username", "Username is already exists");
+                return View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName =model.Username,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LasttName = model.LastName
+            };
+
+
+            var result = await userManager.CreateAsync(user, model.Password);
+            if(!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Roles", error.Description);
+                }
+                return View(model);
+            }
+
+            await userManager.AddToRolesAsync(user, model.Roles.Where(r => r.IsSelected).Select(r => r.RoleName));
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+                return NotFound();
+            var viewModel = new ProfileFormViewModel
+            {
+                Id = userId,
+                FirstName = user.FirstName,
+                LastName = user.LasttName,
+                Username = user.UserName,
+                Email = user.Email
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProfileFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var user = await userManager.FindByIdAsync(model.Id);
+            if (user == null)
+                return NotFound();
+            var userWithSameEmail = await userManager.FindByEmailAsync(model.Email);
+            if(userWithSameEmail != null && userWithSameEmail.Id != model.Id)
+            {
+                ModelState.AddModelError("Email", "this email is already assignet to anther user");
+                return View(model);
+            }
+            var userWithSameUsername = await userManager.FindByNameAsync(model.Username);
+            if (userWithSameUsername != null && userWithSameUsername.Id != model.Id)
+            {
+                ModelState.AddModelError("Email", "this username is already assignet to anther user");
+                return View(model);
+            }
+            user.FirstName = model.FirstName;
+            user.LasttName = model.LastName;
+            user.UserName = model.Username;
+            user.Email = model.Email;
+
+            await userManager.UpdateAsync(user);
+
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
